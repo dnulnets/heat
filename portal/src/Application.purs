c093f5867659eae18cc4ec1837b2126627cbeba4
Tomas Stenlund <tomas.stenlund@telia.com>
2019-04-02 07:08:51 +0200

    Mad the Application Monad more obvious

--- portal/src/Application.purs
+++ portal/src/Application.purs
@@ -7,22 +7,21 @@ module Application where
 
 -- | Language imports
 import Prelude
-import Data.Maybe (Maybe)
 
-import Effect (Effect)
+-- import Effect (Effect)
 import Effect.Aff (Aff)
-import Effect.Aff.Class (class MonadAff, liftAff)
-import Effect.Class (class MonadEffect, liftEffect)
-import Effect.Ref (Ref)
-import Effect.Ref as Ref
+import Effect.Aff.Class (class MonadAff)
+import Effect.Class (class MonadEffect)
+-- import Effect.Ref (Ref)
+-- import Effect.Ref as Ref
 
 import Type.Equality (class TypeEquals, from)
 
-import Control.Monad.Reader (ask, asks, runReaderT)
+import Control.Monad.Reader (asks, runReaderT)
 import Control.Monad.Reader.Class (class MonadAsk)
 import Control.Monad.Reader.Trans (ReaderT)
 
-import Data.Newtype (class Newtype, unwrap)
+import Data.Newtype (class Newtype)
 
 -- | Type of logging to perform
 data LogLevel = Development | Production
@@ -40,22 +39,22 @@ type Environment = {
 -- }
 
 -- | The application monad
-newtype Application a = Application (ReaderT Environment Aff a)
-derive instance newtypeApplication :: Newtype (Application a) _
+newtype ApplicationM a = ApplicationM (ReaderT Environment Aff a)
+derive instance newtypeApplication :: Newtype (ApplicationM a) _
 
 -- | Run the application monad and expose the inner Aff monad
-runApplication :: Environment -> Application ~> Aff
-runApplication env (Application m) = runReaderT m env
+runApplication :: Environment -> ApplicationM ~> Aff
+runApplication env (ApplicationM m) = runReaderT m env
 
 -- | Derive all monad functions
-derive newtype instance functorApplication :: Functor Application
-derive newtype instance applyApplication :: Apply Application
-derive newtype instance applicativeApplication :: Applicative Application
-derive newtype instance bindApplication :: Bind Application
-derive newtype instance monadApplication :: Monad Application
-derive newtype instance monadEffectApplication :: MonadEffect Application
-derive newtype instance monadAffApplication :: MonadAff Application
+derive newtype instance functorApplication :: Functor ApplicationM
+derive newtype instance applyApplication :: Apply ApplicationM
+derive newtype instance applicativeApplication :: Applicative ApplicationM
+derive newtype instance bindApplication :: Bind ApplicationM
+derive newtype instance monadApplication :: Monad ApplicationM
+derive newtype instance monadEffectApplication :: MonadEffect ApplicationM
+derive newtype instance monadAffApplication :: MonadAff ApplicationM
 
 -- | ask implementation
-instance monadAskApplication :: TypeEquals e Environment => MonadAsk e Application where
-  ask = Application $ asks from
+instance monadAskApplication :: TypeEquals e Environment => MonadAsk e ApplicationM where
+  ask = ApplicationM $ asks from
